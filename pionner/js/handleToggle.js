bc87d17336592953a61e55569fd5a2d10e574343
babyazalea <usun12@hotmail.com>
2019-04-29 00:51:43 +0900

    190428 modified

---
+++ pionner/js/handleToggle.js
@@ -0,0 +1,9 @@
+const toggle=document.querySelector('.toggle'),
+    rangeForm=toggle.querySelector('form'),
+    slider=rangeForm.querySelector('input');
+
+    slider.oninput = handlingInput;
+
+    function handlingInput(e){
+        const currentValue = e.target.value;
+    }
